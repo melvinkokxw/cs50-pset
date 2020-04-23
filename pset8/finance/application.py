e6a01826998085a0c4d7959b2aed7928abf55919
Melvin <melvinkokxw@gmail.com>
2020-04-24 05:28:45 +0800

    Add history function
    Add function to show user's transaction history and relevant history 
    webpage

--- pset8/finance/application.py
+++ pset8/finance/application.py
@@ -114,7 +114,10 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+
+    transactions = db.execute("SELECT * FROM transactions WHERE user_id=:id", id=session["user_id"])
+
+    return render_template("history.html", transactions=transactions)
 
 
 @app.route("/login", methods=["GET", "POST"])
