b50be71aa7e46cccd4afa5e220f74442ced71b58
Melvin <melvinkokxw@gmail.com>
2020-04-24 13:24:26 +0800

    Add datetime library to application

--- pset8/finance/application.py
+++ pset8/finance/application.py
@@ -6,6 +6,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 from helpers import apology, login_required, lookup, usd
 
@@ -22,6 +23,7 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
 
 # Custom filter
 app.jinja_env.filters["usd"] = usd
