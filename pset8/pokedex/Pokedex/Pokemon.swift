62d1b414616200e86029305b76dbf606562278a7
Melvin Kok <melvinkokxw@gmail.com>
2020-12-30 17:24:12 +0800

    Replace pokedex files with given files

--- pset8/pokedex/Pokedex/Pokemon.swift
+++ pset8/pokedex/Pokedex/Pokemon.swift
@@ -1,33 +1,25 @@
-//
-//  Pokemon.swift
-//  Pokedex
-//
-//  Created by Melvin Kok on 29/12/20.
-//
-
 import Foundation
 
-struct PokemonList: Codable {
-    let results: [Pokemon]
+struct PokemonListResults: Codable {
+    let results: [PokemonListResult]
 }
 
-struct Pokemon: Codable {
+struct PokemonListResult: Codable {
     let name: String
-//    let number: Int
     let url: String
 }
 
-struct PokemonData: Codable {
+struct PokemonResult: Codable {
     let id: Int
-    let types: [PokemonTypeEntry]
-}
-
-struct PokemonType: Codable {
     let name: String
-    let url: String
+    let types: [PokemonTypeEntry]
 }
 
 struct PokemonTypeEntry: Codable {
     let slot: Int
     let type: PokemonType
+}
+
+struct PokemonType: Codable {
+    let name: String
 }
--- pset8/pokedex/Pokedex/PokemonViewController.swift
+++ pset8/pokedex/Pokedex/PokemonViewController.swift
@@ -1,56 +1,54 @@
-//
-//  PokemonViewController.swift
-//  Pokedex
-//
-//  Created by Melvin Kok on 29/12/20.
-//
-
 import UIKit
 
-class PokemonViewController : UIViewController {
+class PokemonViewController: UIViewController {
+    var url: String!
+
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var numberLabel: UILabel!
     @IBOutlet var type1Label: UILabel!
     @IBOutlet var type2Label: UILabel!
-    
-    var pokemon: Pokemon!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+
+    func capitalize(text: String) -> String {
+        return text.prefix(1).uppercased() + text.dropFirst()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        nameLabel.text = ""
+        numberLabel.text = ""
         type1Label.text = ""
         type2Label.text = ""
-        
-        let url = URL(string: pokemon.url)
-        guard let u = url else {
-            return
-        }
-        URLSession.shared.dataTask(with: u) { (data, response, error) in
+
+        loadPokemon()
+    }
+
+    func loadPokemon() {
+        URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
             guard let data = data else {
                 return
             }
-            
+
             do {
-                let pokemonData = try JSONDecoder().decode(PokemonData.self, from: data)
-                
+                let result = try JSONDecoder().decode(PokemonResult.self, from: data)
                 DispatchQueue.main.async {
-                    self.nameLabel.text = self.pokemon.name
-                    self.numberLabel.text = String(format: "#%03d", pokemonData.id)
-                    
-                    for typeEntry in pokemonData.types {
+                    self.navigationItem.title = self.capitalize(text: result.name)
+                    self.nameLabel.text = self.capitalize(text: result.name)
+                    self.numberLabel.text = String(format: "#%03d", result.id)
+
+                    for typeEntry in result.types {
                         if typeEntry.slot == 1 {
                             self.type1Label.text = typeEntry.type.name
                         }
                         else if typeEntry.slot == 2 {
-                            self.type2Label.text =  typeEntry.type.name
+                            self.type2Label.text = typeEntry.type.name
                         }
                     }
                 }
             }
             catch let error {
-                print("\(error)")
+                print(error)
             }
-            
         }.resume()
     }
 }
