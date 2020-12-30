f08a4fdf1a635d3e003d018bf4e9c166a93d8968
Melvin Kok <melvinkokxw@gmail.com>
2020-12-30 17:28:14 +0800

    Add missing file

---
+++ pset8/pokedex/Pokedex/PokemonListViewController.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+class PokemonListViewController: UITableViewController {
+    var pokemon: [PokemonListResult] = []
+    
+    func capitalize(text: String) -> String {
+        return text.prefix(1).uppercased() + text.dropFirst()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=151") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let entries = try JSONDecoder().decode(PokemonListResults.self, from: data)
+                self.pokemon = entries.results
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
+        cell.textLabel?.text = capitalize(text: pokemon[indexPath.row].name)
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowPokemonSegue",
+                let destination = segue.destination as? PokemonViewController,
+                let index = tableView.indexPathForSelectedRow?.row {
+            destination.url = pokemon[index].url
+        }
+    }
+}
